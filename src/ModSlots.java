3019c721a5dfac96217a25e4d36f5e8b486c7bd2
yulongtan <swifties@uw.edu>
2016-03-25 03:08:35 -0700

    added get and check method and updated constructor

--- src/ModSlots.java
+++ src/ModSlots.java
@@ -6,24 +6,41 @@ public class ModSlots {
     private int count; // forma currently installed
     private int stars; // total amount of times with forma
 
-    public static final int MAX_FORMA = 8;
-
+    public static final int MAX_FORMA = 9;
+	public static final int MAX_ROWS = 2;
+	public static final int MAX_COLS = 4l
+	
+	// Assumes weapon comes with no polarities
     public ModSlots() {
         this.slots = new Polarity[2][4];
+		for (int row = 0; row < MAX_ROWS; row++) {
+			for (int col = 0; col < MAX_COLS; col++) {
+				this.slots[row][col] = Polarity.NONE;
+			}
+		}
         this.count = 0;
         this.stars = 0;
     }
 
     public void polarize(int row, int column, Polarity p) {
-        if (!isLegalSlot(row, column)) {
-            throw new IllegalArgumentException();
-        }
+        this.checkSlot(row, column);
         this.slots[row][column] = p;
         if (this.count != MAX_FORMA) {
             this.count++;
         }
         this.stars++;
     }
+
+	public void move(int oldRow, int oldCol, int newRow, int newCol) {
+		this.checkSlot(oldRow, oldCol);
+		this.checkSlot(newRow, new Col);
+		// ...
+	}
+
+	public Polarity get(int row, int col) {
+		this.checkSlot(row, col);
+		return this.slots[row][col];
+	}
 
     public int formaCount() {
         return this.count;
@@ -34,6 +51,12 @@ public class ModSlots {
     }
 
     private boolean isLegalSlot(int row, int column) {
-        return row > 0 && row < 2 && column > 0 && column < 5;
+        return row > 0 && row < MAX_ROWS && column > 0 && column < MAX_COLS;
     }
+
+	private void checkSlot(int row, int col) {
+		if (!this.isLegalSlot(row, col)) {
+			throw new IllegalArgumentException();
+		}
+	}
 }
