045c2faef20ee6944b7232138a81a9d01c4ccc01
yulongtan <swifties@uw.edu>
2016-03-26 01:25:36 -0700

    added move and get method

--- src/ModSlots.java
+++ src/ModSlots.java
@@ -34,7 +34,10 @@ public class ModSlots {
 	public void move(int oldRow, int oldCol, int newRow, int newCol) {
 		this.checkSlot(oldRow, oldCol);
 		this.checkSlot(newRow, new Col);
-		// ...
+		Polarity old = this.get(oldRow, oldCol);
+		Polarity temp = this.get(newRow, newCol);
+		this.slots[newRow][newCol] = old;
+		this.slots[oldRow][oldCol] = temp;
 	}
 
 	public Polarity get(int row, int col) {
